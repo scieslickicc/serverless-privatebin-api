efcb1f39fa743f65bfb2e319de7c9629bbc3f170
Staszek Cieslicki <s.cieslicki@clearcode.cc>
2020-04-28 17:42:07 +0200

    update list

--- src/list.js
+++ src/list.js
@@ -9,17 +9,27 @@ export const main = handler(async (event, context) => {
 
   const params = {
     TableName: process.env.tableName,
-    FilterExpression: "userId = :userId AND timeToLive > :now AND telomer > :telomer",
+    // FilterExpression: "userId = :userId AND timeToLive > :now AND telomer > :telomer",
+    FilterExpression: "userId = :userId",
     ExpressionAttributeValues: {
       ":userId": item.body.userId,
-      ":now": item.now,
-      ":telomer": 0,
+      // ":now": item.now,
+      // ":telomer": 0,
     }
   };
 
   const result = await dynamoDb.scan(params);
 
   result.Items.sort((a,b) => (a.createdAt > b.createdAt) ? -1 : ((b.createdAt > a.createdAt) ? 1 : 0));
+  result.Items.forEach((item) => {
+    if (item.telomer === 0 || item.timeToLive < item.now) {
+      delete item.content;
+      delete item.iv;
+      delete item.tag;
+    }
+
+    return item;
+  });
 
   return result.Items;
 });
